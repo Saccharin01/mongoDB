9ad51676e6738af2c9ff96ffeae4990ad03b350d
Saccharin01 <chousik01@gmail.com>
2024-07-17 15:45:05 +0900

    feat | test.js
    json 파일을 모듈로 불러오거나, fs를 이용해서 파일 내의 데이터를 리딩 후 DB에 넣는 로직 실험

--- test.js
+++ test.js
@@ -1,6 +1,33 @@
+const fs = require('fs')
+const book = require("./book.json")
+console.log(book)
+
+// fs.readFile("./book.json","utf-8",(err,data)=>{
+//   if(err){
+//     console.log(err)
+//   } else {
+//     console.log(data)
+
+//   }
+// })
 const {MongoClient} = require("mongodb")
 
-const url = 'mongodb://localhost:27017';
-const client = new MongoClient(url)
+// const url = 'mongodb://localhost:27017';
+// const client = new MongoClient(url)
 
-const dbName = "TestNoSQLBasic"
+// const dbName = "TestNoSQLBasic"
+
+// async function main(){
+//   await client.connect();
+//   console.log("connection Success")
+//   const db = client.db(dbName)
+//   const collection = db.collection("TestDocument")
+//   const insertResult = await collection.insertMany([{ a: 1 }, { a: 2 }, { a: 3 }]);
+//   console.log('Inserted documents =>', insertResult);
+//   return "done"
+// }
+
+// main()
+// .then(console.log())
+// .catch(console.error())
+// .finally(()=>{client.close()})
